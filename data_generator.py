a992dd693b4dc0827e9f2109fdec783e97cc1d29
shibei00 <shibei00@gmail.com>
2015-10-30 16:29:40 +0800

    Generate mh370 data.

--- data_generator.py
+++ data_generator.py
@@ -7,12 +7,12 @@ import nltk.tokenize as nltoken
 
 
 def write_file(name, content):
-    f = open(name)
-    f.write(content)
+    f = open(name, 'w')
+    f.write(content.encode('utf-8'))
     f.close()
 
     
-class DataGenerator(Object):
+class DataGenerator(object):
     def __init__(self, news_table_name, comments_table_name, outpath):
         self.news_table_name = news_table_name
         self.comments_table_name = comments_table_name
@@ -28,15 +28,14 @@ class DataGenerator(Object):
 
     def generate_news_comments(self):
         self._connect()
-        sql_news = 'select id, content from %s where comment_num > 10' % (self.news_table_name,)
+        sql_news = 'select id, content from %s where comment_num > 10 and length(content) > 10' % (self.news_table_name,)
         cur = self.conn.cursor()
         cur.execute(sql_news)
         r_news = cur.fetchall()
-        for news, index in r_news, xrange(len(r_news)):
+        for index, news in enumerate(r_news):
             id = news['id']
             content = news['content']
             sent_tokenize_list = [x.strip() for x in nltoken.sent_tokenize(content) if x.strip()]
-            post_sent_tokenize_list = []
             per_news_content = '\n'.join(sent_tokenize_list)
             per_news_name = os.path.join(self.outpath, 'news_{0}.txt'.format(index,))
             per_comment_name = os.path.join(self.outpath, 'comment_{0}.txt'.format(index,))
@@ -52,13 +51,11 @@ class DataGenerator(Object):
                     comments_list.append(' '.join(l_content))
             per_comment_content = '\n'.join(comments_list)
             write_file(per_news_name, per_news_content)
-            write_file(per_comment_name, per_comment_name)
+            write_file(per_comment_name, per_comment_content)
             print '{0}th news and comments are generated!'.format(index,)
         cur.close()
         self._close()
 
 if __name__=='__main__':
-    dg = DataGenerator('news_mh370', 'comments_mh370', './data/mh370')
+    dg = DataGenerator('news_mh370', 'comment_mh370', './data/mh370')
     dg.generate_news_comments()
-    content = comment['content']
-    self._close()
